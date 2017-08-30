20578b787bb48a55122787a843141083f1e325ee
nalhoussari <nalhoussari@hotmail.com>
2017-08-30 14:32:12 -0700

    initial commit

---
+++ FingerPaint/DrawingView.h
@@ -0,0 +1,22 @@
+//
+//  DrawingView.h
+//  FingerPaint
+//
+//  Created by Noor Alhoussari on 2017-06-10.
+//  Copyright © 2017 Noor Alhoussari. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface DrawingView : UIView
+
+
+//- (void)stroke;
+
+- (void)moveToPoint:(CGPoint)point;
+
+- (void)connectPoint:(CGPoint)point;
+
+
+@end
